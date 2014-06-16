ec7d8ef4d2d5ac5a5524b00bf17d938ee76102a8
Ryan Levick <ryan@6wunderkinder.com>
2014-06-17 00:05:52 +0200

    add more statements

--- Einfach.hs
+++ Einfach.hs
@@ -33,17 +33,24 @@ module MyInterpreter where
   operator :: Parser BinOp
   operator = plus <|> minus <|> times <|> divide
 
+  identifier :: Parser String
+  identifier = do s1 <- letter
+                  s2 <- many alphaNum
+                  return $ s1 : s2
+
   assignment :: Parser Statement
-  assignment = do i <- many1 alphaNum
+  assignment = do i <- identifier
                   _ <- char ':'
                   spaces
                   e <- expr
                   return $ Assignment i e
 
+  statement :: Parser Statement
+  statement = assignment
+
   identExpr :: Parser Expr
-  identExpr = do s1 <- letter
-                 s2 <- many1 alphaNum
-                 return $ IdentExpr $ s1 : s2
+  identExpr = do s <- identifier
+                 return $ IdentExpr s
 
   numExpr :: Parser Expr
   numExpr = do ds <- many1 digit
@@ -51,7 +58,9 @@ module MyInterpreter where
 
   opExpr :: Parser Expr
   opExpr = do left <- numExpr <|> identExpr
+              spaces
               op <- operator
+              spaces
               right <- numExpr <|> identExpr
               return $ OpExpr left op right
 
@@ -63,19 +72,16 @@ module MyInterpreter where
                 _ <- spaces >> char ')'
                 return $ EseqExpr s e
 
-  statement :: Parser Statement
-  statement = undefined
-
   expr :: Parser Expr
-  expr = identExpr <|> opExpr <|> numExpr
+  expr = opExpr <|> identExpr <|> numExpr <|> eseqExpr
 
   main :: IO ()
-  main = case parse assignment "example" inputText of
+  main = case parse expr "example" inputText of
               Left  err -> print err
               Right res -> putStrLn $ "I parsed: '" ++ show res ++ "'"
 
   inputText :: String
-  inputText = "a: 5+3"
+  inputText = "(a: 5+3, a + 10)"
 
   realTest :: String
   realTest = "a := 5 + 3; b := (print(a, a - 1), 10 * a); print(b)"
